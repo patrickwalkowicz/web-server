ce9ce061828bd697ed18b331f3abbad3471f9f1c
Patrick Walkowicz <patrykwalkowicz@gmail.com>
2015-10-09 12:10:02 +0100

    Initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('Private route hit');
+		next();
+	},
+	logger: function(req, res, next) {
+
+		console.log('Request: ' + req.method + ' ' +req.originalUrl + ' ' + new Date().toString());
+		next();
+	}
+}
+	
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+// routing
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(port, function() {
+	console.log("Server started");
+	console.log("Port: " + port)
+});
